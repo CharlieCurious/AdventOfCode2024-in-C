4fc009fcdd240f6b399b03740db1607dfae65111
CharlieCurious <dfrco2@outlook.com>
2024-12-01 22:05:33 +0200

    No more min queue. Using qsort.

--- solutions/day_1/src/main.c
+++ solutions/day_1/src/main.c
@@ -1,7 +1,10 @@
 #include <dictionary.h>
 #include <file_parser.h>
-#include <min_heap.h>
 #include <stdlib.h>
+
+#define INPUT_LINES_COUNT 1000
+
+static int qsort_compare(const void *a, const void *b);
 
 int main() {
     FILE *file = open_input_file("input_files/day_1.txt"); 
@@ -11,43 +14,59 @@ int main() {
     read_input_lines(file, input_lines);
     fclose(file);
 
-    // wdf did I use min heaps?! shouldn've just sorted...
-    MinHeap *left_col = create_min_heap(1000);
-    MinHeap *right_col = create_min_heap(1000);
+    unsigned int left_col[INPUT_LINES_COUNT];
+    unsigned int right_col[INPUT_LINES_COUNT];
+
     Dictionary *multipliers = create_dictionary(1000);
 
-    for (unsigned int i = 0; i < input_lines->count; i++) {
+    for (unsigned int i = 0; i < INPUT_LINES_COUNT; i++) {
         unsigned int left_num, right_num;
         sscanf(input_lines->lines[i], "%u %u", &left_num, &right_num);
-
-        insert(left_col, left_num);
-        insert(right_col, right_num);
+        
+        left_col[i] = left_num;
+        right_col[i] = right_num;
 
         unsigned int occurence_count = 0;
         find(multipliers, right_num, &occurence_count);
         insert_in_dictionary(multipliers, right_num, ++occurence_count);
     }
 
-    unsigned int sums[1000];
-    unsigned int multiplied_sums[1000];
-    for (unsigned int i = 0; i < 1000; i++) {
-        unsigned int left_num = pop_min(left_col);
+    qsort(left_col, INPUT_LINES_COUNT, sizeof(unsigned int), qsort_compare);
+    qsort(right_col, INPUT_LINES_COUNT, sizeof(unsigned int), qsort_compare);
 
-        unsigned int diff = abs((int)left_num - (int)pop_min(right_col));
+    unsigned int sums[INPUT_LINES_COUNT];
+    unsigned int multiplied_sums[INPUT_LINES_COUNT];
+    for (unsigned int i = 0; i < INPUT_LINES_COUNT; i++) {
+
+        unsigned int diff = abs((int)left_col[i] - (int)right_col[i]);
         sums[i] = diff;
 
         unsigned int factor = 0; 
-        find(multipliers, left_num, &factor);
-        multiplied_sums[i] = left_num * factor;
+        find(multipliers, left_col[i], &factor);
+        multiplied_sums[i] = left_col[i] * factor;
     }
 
     unsigned int sum = 0;
     unsigned int sum_with_factors = 0;
-    for (unsigned int i = 0; i < 1000; i++) {
+    for (unsigned int i = 0; i < INPUT_LINES_COUNT; i++) {
         sum += sums[i];
         sum_with_factors += multiplied_sums[i];
     }
 
-    printf("Part 1 solution is: %u\n. Part 2 solution is: %u\n", sum, sum_with_factors);
-
+    printf("Part 1 solution is: %u\nPart 2 solution is: %u\n", sum, sum_with_factors);
 }
+
+static int qsort_compare(const void *a, const void *b) {
+    return *(unsigned int *)a - *(unsigned int *)b;
+}
+
+
+
+
+
+
+
+
+
+
+
