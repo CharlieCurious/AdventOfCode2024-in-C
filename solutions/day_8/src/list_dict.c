2e6f9282c1405077336664408f1dd0d501822847
CharlieCurious <dfrco2@outlook.com>
2024-12-14 22:40:54 +0200

    [day 8] working on part 1 dict

--- solutions/day_8/src/list_dict.c
+++ solutions/day_8/src/list_dict.c
@@ -1,6 +1,10 @@
 #include <list_dict.h>
 #include <stdlib.h>
 
+static void free_key_value(KeyValue *kv);
+static void free_locations_list(LocationList *list);
+static size_t hash(char key, size_t capacity);
+
 AnthenasDictionary *dictionary_create(uint16_t capacity) {
     AnthenasDictionary *dict = (AnthenasDictionary *)malloc(sizeof(AnthenasDictionary));
     if (!dict) 
@@ -21,12 +25,46 @@ return_null:
     return NULL;
 }
 
-bool dictionary_insert(char symbol, Location location);
+bool dictionary_insert(AnthenasDictionary *dict, char key, Location location) {
+    if (!dict)
+        return false;
 
-void dictionary_free(AnthenasDictionary *dict) {
-    for (uint16_t i = 0; i < dict->capacity; i++) {
-        if (dict->kvp[i]) {
-            for 
+    KeyValue *kv = dict->kvp[hash(key, dict->capacity)];
+    while (kv) {
+        if (kv->key == key) {
+            kv->value = location;
         }
     }
+}
+
+void dictionary_free(AnthenasDictionary *dict) {
+    if (!dict)
+        return;
+
+    for (size_t i = 0; i < dict->capacity; i++) {
+        free_key_value(dict->kvp[i]);
+    }
+    free(dict);
+}
+
+static void free_key_value(KeyValue *kv) {
+    if (!kv)
+        return;
+    KeyValue *current = kv->next;
+    while(current) {
+        free_locations_list(current->value);
+        free(current);
+        current = current->next;
+    }
+    free(kv);
+}
+
+static void free_locations_list(LocationList *list) {
+    if (!list)
+        return;
+    free(list);
+}
+
+static size_t hash(char key, size_t capacity) {
+    return (key * 31) % capacity;
 }
